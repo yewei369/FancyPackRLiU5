1febc8e6639cbc09e168270126081321cde83e13
Ismail Khalil <ismailkhalil2@gmail.com>
2019-10-01 16:26:38 +0100

    adding content and httr function

--- Try1.R
+++ Try1.R
@@ -1,20 +1,24 @@
 #GET" request to the API to pull raw data
 
 
-http://api.thenmap.net/v2/us-4/data/2019-10-01?language=en&data_props=name|capital|is_in|etc...
 
-<div id="map"></div>
-  <script src="//drvkoaf101245.cloudfront.net/thenmap-2.0.0.min.js"></script>
-    var settings = {
-      dataKey: "",
-      dataset: "us-4",
-      date: "2019-10-01"
-      language: "en",
-      projection: "albersUsa",
-      width:900,
-      height:900
-    };
-    Thenmap.init("map", settings);
-    r <- GET("http://api.thenmap.net/v2/us-4/geo/2019-10-01")
-    r
     
+    r <- GET("http://api.thenmap.net/v2/us-4/geo/2019-10-01")
+   #Response Object
+    r 
+     
+     #Status code 
+     status_code(r)
+#200 means successful
+     
+     headers(r)
+     
+#Content
+     content(r, "text")
+     content(r, "raw")
+     content(r, "parsed")
+     str(content(r))
+
+ # http status 
+     http_status(r)
+     

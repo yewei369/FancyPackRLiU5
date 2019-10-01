12f28f0ef65f3ae81e6e65982ce03ca5e4bf9261
Ismail Khalil <ismailkhalil2@gmail.com>
2019-10-01 16:47:31 +0100

    finnaly transformed the json file into dataframe

--- Try1.R
+++ Try1.R
@@ -1,3 +1,5 @@
+library(httr)
+library(jsonlite)
 #GET" request to the API to pull raw data
 
 
@@ -22,3 +24,8 @@
  # http status 
      http_status(r)
      
+     
+    # parse the data to jason format 
+     r_json <- fromJSON("http://api.thenmap.net/v2/us-4/geo/2019-10-01", flatten = TRUE)
+     r_df <- as.data.frame(r_json)
+  View(r_df)
